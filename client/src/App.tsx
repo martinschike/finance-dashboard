2711215025d8e87e6fb998b9748e5b3883f56a88
Martins Okafor <okaformartinschinwike@gmail.com>
2023-04-04 20:46:41 +0100

    Dashboard setup as responsive

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,29 @@
+import {ThemeProvider, CssBaseline} from "@mui/material"
+import { createTheme } from "@mui/material/styles";
+import { Box } from "@mui/material";
+import { useMemo } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {themeSettings} from "./theme";
+import Navbar from "@/scenes/navbar";
+import Dashboard from "@/scenes/dashboard";
 
 function App() {
-
+  const theme = useMemo(() => createTheme(themeSettings), [])
 
   return (
     <div className="app">
-     
+      <BrowserRouter>
+     <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/predictions" element={<div>predictions page</div>} />
+          </Routes>
+        </Box>
+     </ThemeProvider>
+     </BrowserRouter>
     </div>
   )
 }
